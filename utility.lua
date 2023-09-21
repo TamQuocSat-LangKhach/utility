0eb2fdb83dd7193788e2f12f069f1ae4871da4e4
notify <notify-ctrl@qq.com>
2023-09-21 13:30:17 +0800

    init

---
+++ utility.lua
@@ -0,0 +1,9 @@
+-- 这个文件用来堆一些常用的函数，视情况可以分成多个文件
+-- 因为不是拓展包，所以不能用init.lua（否则会被当拓展包加载并报错）
+-- 因为本体里面已经有util了所以命名为完整版utility
+
+-- 要使用的话在拓展开头加一句local U = require "packages/utility/utility.lua" 即可
+
+local Utility = {}
+
+return Utility
