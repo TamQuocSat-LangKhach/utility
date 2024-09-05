0b2146a7fb4a8d5d6d4122ba1cf4ec4107dc453e
RalphR <1715333874@qq.com>
2024-09-05 09:29:55 +0800

    移入仁区相关

--- mobile_util.lua
+++ mobile_util.lua
@@ -22,9 +22,6 @@ Fk:loadTranslationTable{
   ["@skill_charge"] = "蓄力",
 }
 
-
-
-
 --- 进行谋奕/对策
 ---@param room Room @ 房间
 ---@param from ServerPlayer @ 拥有技能的角色
@@ -86,7 +83,168 @@ Fk:loadTranslationTable{
   ["#StrategiesResult"] = "%from 对 %to %arg",
 }
 
+--仁区相关
 
+---@param room Room @ 房间
+---@return integer[]
+Utility.GetRenPile = function(room)
+  room.tag["ren"] = room.tag["ren"] or {}
+  return table.simpleClone(room.tag["ren"])
+end
+
+---@param room Room @ 房间
+Utility.NotifyRenPile = function(room)
+  room:sendLog{
+    type = "#NotifyRenPile",
+    arg = #Utility.GetRenPile(room),
+    card = Utility.GetRenPile(room),
+  }
+  room:setBanner("@$RenPile", table.simpleClone(room.tag["ren"]))
+end
+
+---@param room Room @ 房间
+---@param card integer|integer[]|Card|Card[] @ 要加入仁区的牌/id/intList
+---@param skillName string @ 移动的技能名
+---@param proposer integer @ 移动操作者的id
+Utility.AddToRenPile = function(room, card, skillName, proposer)
+  local ids = Card:getIdList(card)
+  room.tag["ren"] = room.tag["ren"] or {}
+  local add, remove = {}, {}
+  local ren_cards = table.simpleClone(room.tag["ren"])
+  local ren_limit = 6
+  for i = 1, ren_limit do
+    local id = ids[i]
+    if not id then break end
+    table.insert(ren_cards, id)
+    table.insert(add, id)
+  end
+  if #ren_cards > ren_limit then
+    for i = #ren_cards - ren_limit, 1, -1 do
+      table.insert(remove, table.remove(room.tag["ren"], i))
+    end
+  end
+  local moveInfos = {}
+  if #add > 0 then
+    table.insertTable(room.tag["ren"], add)
+    table.insert(moveInfos, {
+      ids = add,
+      from = room.owner_map[add[1]],
+      toArea = Card.Void,
+      moveReason = fk.ReasonJustMove,
+      skillName = skillName,
+      moveVisible = true,
+      proposer = proposer,
+    })
+    room:sendLog{
+      type = "#AddToRenPile",
+      arg = #add,
+      card = add,
+    }
+  end
+  if #remove > 0 then
+    local info = {
+      ids = remove,
+      toArea = Card.DiscardPile,
+      moveReason = fk.ReasonPutIntoDiscardPile,
+      skillName = "ren_overflow",
+      moveVisible = true,
+    }
+    table.insert(moveInfos, info)
+    room.logic:trigger("fk.BeforeRenMove", nil, info)
+    room:sendLog{
+      type = "#OverflowFromRenPile",
+      arg = #remove,
+      card = remove,
+    }
+  end
+  if #moveInfos > 0 then
+    room:moveCards(table.unpack(moveInfos))
+    for _, info in ipairs(moveInfos) do
+      if info.skillName == "ren_overflow" then
+        room.logic:trigger("fk.AfterRenMove", nil, info)
+      end
+    end
+  end
+
+  Utility.NotifyRenPile(room)
+end
+
+---@param room Room @ 房间
+---@param player ServerPlayer @ 获得牌的角色
+---@param cid integer | integer[] | Card | Card[] @ 要获得的牌
+---@param skillName string @ 技能名
+Utility.GetCardFromRenPile = function(room, player, cid, skillName)
+  skillName = skillName or ""
+  cid = Card:getIdList(cid)
+  if #cid == 0 then return end
+  local move = {
+    ids = cid,
+    to = player.id,
+    toArea = Card.PlayerHand,
+    moveReason = fk.ReasonJustMove,
+    proposer = player.id,
+    moveVisible = true,
+    skillName = skillName,
+  }
+  room.logic:trigger("fk.BeforeRenMove", nil, move)
+  room:moveCards(move)
+  room.logic:trigger("fk.AfterRenMove", nil, move)
+  for _, id in ipairs(cid) do
+    table.removeOne(room.tag["ren"], id)
+  end
+  room:sendLog{
+    type = "#GetCardFromRenPile",
+    from = player.id,
+    arg = #cid,
+    card = cid,
+  }
+  Utility.NotifyRenPile(room)
+end
+
+---@param room Room @ 房间
+---@param player ServerPlayer @ 弃置牌的角色
+---@param ids integer|integer[] @ 要弃置的id/idList
+---@param skillName string @ 技能名
+Utility.DiscardCardFromRenPile = function(room, player, ids, skillName)
+  skillName = skillName or ""
+  if type(ids) ~= "number" then
+    ids = ids
+  else
+    ids = {ids}
+  end
+  if #ids == 0 then return end
+  local move = {
+    ids = ids,
+    toArea = Card.DiscardPile,
+    moveReason = fk.ReasonDiscard,
+    proposer = player.id,
+    moveVisible = true,
+    skillName = skillName,
+  }
+  room.logic:trigger("fk.BeforeRenMove", nil, move)
+  room:moveCards(move)
+  room.logic:trigger("fk.AfterRenMove", nil, move)
+  for _, id in ipairs(ids) do
+    table.removeOne(room.tag["ren"], id)
+  end
+  room:sendLog{
+    type = "#DiscardCardFromRenPile",
+    from = player.id,
+    arg = #ids,
+    card = ids,
+  }
+  Utility.NotifyRenPile(room)
+end
+
+Fk:loadTranslationTable{
+  ["#NotifyRenPile"] = "“仁”区现有 %arg 张牌 %card",
+  ["#AddToRenPile"] = "%arg 张牌被移入“仁”区 %card",
+  ["#OverflowFromRenPile"] = "%arg 张牌从“仁”区溢出 %card",
+  ["#GetCardFromRenPile"] = "%from 从“仁”区获得 %arg 张牌 %card",
+  ["#DiscardCardFromRenPile"] = "%from 弃置了“仁”区 %arg 张牌 %card",
+  ["$RenPile"] = "仁区",
+  ["@$RenPile"] = "仁区",
+}
 
 
 
