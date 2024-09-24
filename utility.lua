b38158f27763411b96a688c1c6cb4df11255c39b
seven <786852516@qq.com>
2024-09-24 18:00:48 +0800

    使用手牌check

--- utility.lua
+++ utility.lua
@@ -1205,34 +1205,35 @@ Fk:loadTranslationTable{
 
 
 
---- 判断当前使用/打出事件是否是在使用手牌
+--- 判断使用/打出事件是否是在使用手牌
 ---@param player ServerPlayer @ 要判断的使用者
----@param data CardUseStruct @ 当前事件的data
+---@param data CardUseStruct|integer @ 当前事件的data，或需要查询的使用事件的id
 ---@return boolean
 Utility.IsUsingHandcard = function(player, data)
-  local cards = data.card:isVirtual() and data.card.subcards or {data.card.id}
+  local useEvent, cards
+  if type(data) == "number" then
+    useEvent = player.room.logic.all_game_events[data]
+    cards = Card:getIdList(useEvent.data[1].card)
+  else
+    useEvent = player.room.logic:getCurrentEvent()
+    cards = Card:getIdList(data.card)
+  end
   if #cards == 0 then return false end
-  local yes = true
-  local use = player.room.logic:getCurrentEvent()
-  use:searchEvents(GameEvent.MoveCards, 1, function(e)
-    if e.parent and e.parent.id == use.id then
-      local subcheck = table.simpleClone(cards)
-      for _, move in ipairs(e.data) do
-        if (move.moveReason == fk.ReasonUse or move.moveReason == fk.ReasonResonpse) then
-          for _, info in ipairs(move.moveInfo) do
-            if table.removeOne(subcheck, info.cardId) and info.fromArea ~= Card.PlayerHand then
-              yes = false
-              return true
-            end
-          end
+  local moveEvents = useEvent:searchEvents(GameEvent.MoveCards, 1, function(e)
+    return e.parent and e.parent.id == useEvent.id
+  end)
+  if #moveEvents == 0 then return false end
+  local subcheck = table.simpleClone(cards)
+  for _, move in ipairs(moveEvents[1].data) do
+    if (move.moveReason == fk.ReasonUse or move.moveReason == fk.ReasonResonpse) then
+      for _, info in ipairs(move.moveInfo) do
+        if table.removeOne(subcheck, info.cardId) and info.fromArea ~= Card.PlayerHand then
+          return false
         end
       end
-      if #subcheck == 0 then
-        return true
-      end
     end
-  end)
-  return yes
+  end
+  return #subcheck == 0
 end
 
 
