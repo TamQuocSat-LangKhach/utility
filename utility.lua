97f7da6dadc502f3aed5c3f8b8cd232f5fe449d6
xxyheaven <1433191064@qq.com>
2025-02-08 23:37:53 +0800

    fix

--- utility.lua
+++ utility.lua
@@ -2931,12 +2931,12 @@ local choose_cards_mutlipat_skill = fk.CreateActiveSkill{
   interaction = function(self)
     local patterns = self.patterns
     if not patterns then return end
-    return {choices = table.map(patterns, function(v, i) return v[4] end) }
+    return UI.ComboBox {choices = table.map(patterns, function(v, i) return v[4] end) }
   end,
-  card_filter = function(self, to_select, selected)
+  card_filter = function(self, to_select, selected, player)
     local patterns, choice = self.patterns, self.interaction.data
     if not patterns or not choice then return end
-    if self.discard_skill or Self:prohibitDiscard(to_select) then return end
+    if self.discard_skill and player:prohibitDiscard(to_select) then return end
     for _, v in ipairs(patterns) do
       if v[4] == choice then
         return #selected < v[3] and Exppattern:Parse(v[1]):match(Fk:getCardById(to_select))
