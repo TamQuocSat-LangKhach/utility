e6564d5bc47702ad7a644f9678a7f830e69adcab
RalphR <1715333874@qq.com>
2025-04-23 21:19:02 +0800

    赠予

--- utility.lua
+++ utility.lua
@@ -1149,7 +1149,7 @@ Utility.FamilyMember = function (player, target)
     ["yingchuan_han"] = {"hanshao", "hanrong"},
     ["taiyuan_wang"] = {"wangyun", "wangling", "wangchang", "wanghun", "wanglun", "wangguang", "wangmingshan", "wangshen"},
     ["yingchuan_zhong"] = {"zhongyao", "zhongyu", "zhonghui", "zhongyan"},
-    ["hongnong_yang"] = {"yangci", "yangbiao", "yangxiu", "yangjun", "yangyan", "yangzhi"},
+    ["hongnong_yang"] = {"yangci", "yangbiao", "yangxiu", "yangzhongh", "yangjun", "yangyan", "yangzhi"},
     ["wujun_lu"] = {"luji", "luxun", "lukang", "luyusheng", "lukai"},
   }
   for f, members in pairs(familyMap) do
@@ -1346,50 +1346,79 @@ MarkEnum.SlashBypassTimes = "__slash_by_times"
 ---使用杀无距离限制，可带后缀
 MarkEnum.SlashBypassDistances = "__slash_by_dist"
 
+
+--- PresentCardData 赠予数据
+---@class PresentCardDataSpec
+---@field public from ServerPlayer @ 发起赠予的玩家
+---@field public to ServerPlayer @ 接受赠予的玩家
+---@field public card Card @ 赠予的牌
+---@field public skill_name? string @ 技能名
+
+---@class Utility.PresentCardData: PresentCardDataSpec, TriggerData
+Utility.PresentCardData = TriggerData:subclass("PresentCardData")
+
+--- TriggerEvent
+---@class Utility.PresentCardTriggerEvent: TriggerEvent
+---@field public data Utility.PresentCardData
+Utility.PresentCardTriggerEvent = TriggerEvent:subclass("PresentCardEvent")
+
+--- 赠予前，用于增加赠予失败参数
+---@class Utility.BeforePresentCard: Utility.PresentCardTriggerEvent
+Utility.BeforePresentCard = Utility.PresentCardTriggerEvent:subclass("Utility.BeforePresentCard")
+--- 赠予失败时
+---@class Utility.PresentCardFail: Utility.PresentCardTriggerEvent
+Utility.PresentCardFail = Utility.PresentCardTriggerEvent:subclass("Utility.PresentCardFail")
+--- 赠予后
+---@class Utility.BeforePresentCard: Utility.PresentCardTriggerEvent
+Utility.AfterPresentCard = Utility.PresentCardTriggerEvent:subclass("Utility.AfterPresentCard")
+
+---@alias PresentCardTrigFunc fun(self: TriggerSkill, event: Utility.PresentCardTriggerEvent,
+---  target: ServerPlayer, player: ServerPlayer, data: Utility.PresentCardData):any
+
+---@class SkillSkeleton
+---@field public addEffect fun(self: SkillSkeleton, key: Utility.PresentCardTriggerEvent,
+---  data: TrigSkelSpec<PresentCardTrigFunc>, attr: TrigSkelAttribute?): SkillSkeleton
+
 --- 赠予
 ---@param player ServerPlayer @ 交出牌的玩家
 ---@param target ServerPlayer @ 获得牌的玩家
----@param card integer|Card @ 赠予的牌
+---@param card integer|Card @ 赠予的牌，一次只能赠予一张
+---@param skill_name? string @ 技能名
 ---@return boolean
-Utility.presentCard = function(player, target, card)
+Utility.presentCard = function(player, target, card, skill_name)
   local room = player.room
+  assert(#Card:getIdList(card) == 1)
   if type(card) == "number" then
     card = Fk:getCardById(card)
   end
-  if room.logic:trigger("fk.BeforePresentCardFail", player, {to = target, card = card}) then
-    room.logic:trigger("fk.PresentCardFail", player, {to = target, card = card})
-    room:moveCardTo(card, Card.DiscardPile, target, fk.ReasonJustMove, "present", nil, true, player.id)
+  local data = {
+    from = player,
+    to = target,
+    card = card,
+    skill_name = skill_name,
+  }
+  room.logic:trigger(Utility.BeforePresentCard, player, data)
+  if data.fail then
+    room.logic:trigger(Utility.PresentCardFail, player, data)
+    if table.contains(player:getCardIds("h"), card.id) then
+      room:moveCardTo(card, Card.DiscardPile, player, fk.ReasonPutIntoDiscardPile, "present", nil, true, player)
+    end
     return false
   end
   if card.type ~= Card.TypeEquip then
-    room:moveCardTo(card, Card.PlayerHand, target, fk.ReasonGive, "present", nil, true, player.id)
+    data.fail = false
+    room:moveCardTo(card, Card.PlayerHand, target, fk.ReasonGive, "present", nil, true, player)
   else
-    if target:hasEmptyEquipSlot(card.sub_type) then
-      room:moveCardTo(card, Card.PlayerEquip, target, fk.ReasonGive, "present", nil, true, player.id)
-    elseif #target:getEquipments(card.sub_type) > 0 then
-      room:moveCards({
-        ids = target:getEquipments(card.sub_type),
-        from = target.id,
-        toArea = Card.DiscardPile,
-        moveReason = fk.ReasonJustMove,
-        skillName = "present",
-        proposer = player.id,
-      },
-      {
-        ids = {card.id},
-        from = player.id,
-        to = target.id,
-        toArea = Card.PlayerEquip,
-        moveReason = fk.ReasonJustMove,
-        skillName = "present",
-        proposer = player.id,
-      })
-    elseif #target:getAvailableEquipSlots(card.sub_type) == 0 then
-      room.logic:trigger("fk.PresentCardFail", player, {to = target, card = card})
-      room:moveCardTo(card, Card.DiscardPile, target, fk.ReasonJustMove, "present", nil, true, player.id)
+    local ret = room:moveCardIntoEquip(target, card.id, "present", true, player)
+    if #ret == 0 then
+      data.fail = true
+      room.logic:trigger(Utility.PresentCardFail, player, data)
       return false
+    else
+      data.fail = false
     end
   end
+  room.logic:trigger(Utility.AfterPresentCard, player, data)
   return true
 end
 
